052106147933704fbdb9cfbcf877feb340c4d49d
kingchamp3 <kingchamp3@users.noreply.github.com>
2026-07-05 20:50:27 +0900

    Use cache-busted Firebase config file

---
+++ web/firebase-config-9ec46.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDOyMjBDYhflclTePn7Dfxxcw65xlrqbhQ",
+  authDomain: "bible-9ec46.firebaseapp.com",
+  projectId: "bible-9ec46",
+  storageBucket: "bible-9ec46.firebasestorage.app",
+  messagingSenderId: "302546089169",
+  appId: "1:302546089169:web:190ada5b7beb73f78f81f0",
+  measurementId: "G-5277Q2TX28",
+};
